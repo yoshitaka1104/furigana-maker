5b9613ec6bfb5c4b59af2a7b6eff52bbc49a6c3c
Yoshitaka <yymnk1104@gmail.com>
2026-04-17 08:08:23 +0900

    不要なコピー機能の削除と動作の軽量化（最終安定版）

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/kuromoji/dict',
-          dest: ''
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       path: 'path-browserify',
